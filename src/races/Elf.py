9f9695604e9517918e3bf94e9214ccf453631c79
JosephBerman <berman.jos@northeastern.edu>
2021-04-02 19:59:27 -0400

    Finilizing how the Races are going to be handled

--- src/races/Elf.py
+++ src/races/Elf.py
@@ -7,10 +7,27 @@ class raceElf(IRace):
         self.__speed = 35
         self.__statBonus1 = "Wisdom"
         self.__statBonus2 = "Dexterity"
-        pass
+
+        super().__init__()
 
     def printRace(self):
         print(self.__name, self.__speed, self.__statBonus1, self.__statBonus2)
 
+    def getName(self):
+        return self.__name
+
+    def getSpeed(self):
+        return self.__speed
+
+    def getStatBonus1(self):
+        return self.__statBonus1
+
+    def getStatBonus2(self):
+        return self.__statBonus2
+
+
+
 elf = raceElf()
 elf.printRace()
+
+
--- src/races/IRace.py
+++ src/races/IRace.py
@@ -1,12 +1,25 @@
 import abc
+import inspect
 
 class IRace(abc.ABC):
+
 
     @abc.abstractmethod
     def printRace(self):
         pass
 
-    __name: str
-    __speed: int
-    __statBonus1: str
-    __statBonus2: str
+    @abc.abstractmethod
+    def getName(self):
+        pass
+
+    @abc.abstractmethod
+    def getSpeed(self):
+        pass
+
+    @abc.abstractmethod
+    def getStatBonus1(self):
+        pass
+
+    @abc.abstractmethod
+    def getStatBonus2(self):
+        pass
