03ac948245a6b77d10d2bddc7d9af604945f4c33
JosephBerman <berman.jos@northeastern.edu>
2021-04-02 17:58:52 -0400

    Adding API folder

--- src/character.py
+++ src/character.py
@@ -1,9 +1,5 @@
 class Character:
     def __init__(self):
         self.data = []
-
-    def testing(self):
-        print("epic")
-
-testing = Character()
-testing.testing()
+    race = 0  # going to make a race class
+    characterClass = 0  # going to make a class class
