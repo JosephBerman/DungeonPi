ef9c231c2fcaeba08ad1f2a877915adfa11fad05
JosephBerman <berman.jos@northeastern.edu>
2021-04-04 14:26:18 -0400

    Added stats

--- src/characters/Character.py
+++ src/characters/Character.py
@@ -1,14 +1,21 @@
 from src.characterClass.ProtagonistClasses import *
 from src.races.ProtagonistRaces import *
+from src.characters.Stats import *
 
 
 class Character:
-    def __init__(self):
-        self.characterRace = Human()  # going to make a race class
-        self.characterClass = Archer()  # going to make a class class
+    def __init__(self, cr: CharacterRace, cc: CharacterClass, st: Stats):
+        self._characterRace = cr
+        self._characterClass = cc
+        self._stats = st
+        self._stats.statBonus1(self._characterRace.getStatBonus1())
+        self._stats.statBonus2(self._characterRace.getStatBonus2())
 
     def printCharacter(self):
-        self.characterRace.printRace()
-        self.characterClass.printClass()
+        self._characterRace.printRace()
+        self._characterClass.printClass()
+        self._stats.printStats()
 
 
+testing = Character(Elf(), Monk(), Stats(1, 1, 1, 1, 1, 1))
+testing.printCharacter()
---
+++ src/characters/Stats.py
@@ -0,0 +1,58 @@
+class Stats:
+    def __init__(self, st: int, dx: int, cn: int, il: int, ws: int, ca: int):
+        self._stre = st
+        self._dex = dx
+        self._con = cn
+        self._intel = il
+        self._wis = ws
+        self._cha = ca
+
+    def printStats(self):
+        print("str:", self._stre, "dex:", self._dex,
+              "con:", self._con, "int:", self._intel, "wis:", self._wis, "cha:", self._cha)
+
+    def getStre(self):
+        return self._stre
+
+    def getDex(self):
+        return self._dex
+
+    def getCon(self):
+        return self._con
+
+    def getIntel(self):
+        return self._intel
+
+    def getWis(self):
+        return self._wis
+
+    def getCha(self):
+        return self._cha
+
+    def statBonus1(self, bn: str):
+        if bn == "Strength":
+            self._stre += 2
+        elif bn == "Dexterity":
+            self._dex += 2
+        elif bn == "Constitution":
+            self._con += 2
+        elif bn == "Intelligence":
+            self._intel += 2
+        elif bn == "Wisdom":
+            self._wis += 2
+        elif bn == "Charisma":
+            self._cha += 2
+
+    def statBonus2(self, bn: str):
+        if bn == "Strength":
+            self._stre += 1
+        elif bn == "Dexterity":
+            self._dex += 1
+        elif bn == "Constitution":
+            self._con += 1
+        elif bn == "Intelligence":
+            self._intel += 1
+        elif bn == "Wisdom":
+            self._wis += 1
+        elif bn == "Charisma":
+            self._cha += 1
