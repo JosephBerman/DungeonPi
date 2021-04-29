7a331679b9f5fd0ea818c01dbdc2da3577897f5b
JosephBerman <berman.jos@northeastern.edu>
2021-04-29 18:20:03 -0400

    It now generates a single room! Lets go!

--- src/map/Map.py
+++ src/map/Map.py
@@ -1,5 +1,8 @@
 from src.map.room.Room import *
 from src.map.room.Wall import *
+import random
+
+random.seed()
 
 
 class Map:
@@ -8,10 +11,21 @@ class Map:
         self._length = ln
         self._level = [[EmptyRoom() for i in range(self._width)] for j in range(self._length)]
 
+    def generateMap(self):
+        origin = [(int(self._width / 2)), self._length -1]
+        print(origin[0])
+        print(origin[1])
+        self.addRoom(Room(), origin[1], origin[0])
+
     def printMap(self):
         for i in range(self._length):
             print(self._level[i])
 
+    def addRoom(self, rm: Room, x: int, y: int):
+        self._level[x][y] = rm
 
-testing = Map(3,3)
+
+testing = Map(3, 2)
+test = Room()
+testing.generateMap()
 testing.printMap()
