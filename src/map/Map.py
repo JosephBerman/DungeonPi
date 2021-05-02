84ff237cac7fea45cc49ca8a3ae5cee0732d322b
JosephBerman <berman.jos@northeastern.edu>
2021-05-01 21:06:26 -0400

    Still trying to figure out where my index out of bounds issue is occuring

--- src/map/Map.py
+++ src/map/Map.py
@@ -15,8 +15,7 @@ class Map:
 
     def generateMap(self):
         origin = [(int(self._xcord / 2)), self._ycord - 1]
-        print(origin[0])
-        print(origin[1])
+        print(origin)
         self.generateMapRooms(CONSTANT.ORIGIN, CONSTANT.ROOMRATE, origin)
 
     def generateMapRooms(self, dir: str, rate: int, cords: list):
@@ -29,25 +28,30 @@ class Map:
         elif dir == CONSTANT.SOUTH:
             return
         elif dir == CONSTANT.ORIGIN:
+            self.addRoom(Room(), cords[1], cords[0])
             while True:
-                self.addRoom(Room(), cords[1], cords[0])
-                direction = self.generateMapRoomsDirections(random.randint(1, 4))
+                direction = self.generateMapRoomsDirections(random.randint(0, 3))
                 newCords = self.generateMapsNewCords(direction, cords)
-                if self.generateMapRoomsEdgeCheck(direction, newCords):
+                print(newCords)
+                if self.generateMapRoomsEdgeCheck(direction, cords) and direction is not CONSTANT.SOUTH:
+                    print("test")
                     break
             self.addRoom(Room(), newCords[1], newCords[0])
+            if self.newRate(rate):
+                self.generateMapRooms(direction, rate - 50, newCords)
+
         elif dir == CONSTANT.NONE:
             return
 
 
     def generateMapRoomsEdgeCheck(self, dir: str, cords: list):
-        if (cords[1] == self._ycord) and (dir == CONSTANT.SOUTH):  # Highest Y
+        if (cords[1] >= self._ycord) and (dir == CONSTANT.SOUTH):  # Highest Y
             return True
-        elif (cords[1] == 0) and (dir == CONSTANT.NORTH):  # Lowest Y
+        elif (cords[1] <= 0) and (dir == CONSTANT.NORTH):  # Lowest Y
             return True
-        elif (cords[0] == self._xcord) and (dir == CONSTANT.EAST):  # Highest X
+        elif (cords[0] >= self._xcord) and (dir == CONSTANT.EAST):  # Highest X
             return True
-        elif (cords[0] == 0) and (dir == CONSTANT.WEST):  # Lowest X
+        elif (cords[0] <= 0) and (dir == CONSTANT.WEST):  # Lowest X
             return True
         elif type(self._level[cords[1]][cords[0]]) is not EmptyRoom:  # If a room already exists
             return True
@@ -56,25 +60,34 @@ class Map:
 
     def generateMapRoomsDirections(self, num: int) -> str:
         if num == 0:
+            print("North")
             return CONSTANT.NORTH
         elif num == 1:
+            print("East")
             return CONSTANT.EAST
         elif num == 2:
+            print("South")
             return CONSTANT.SOUTH
         elif num == 3:
+            print("West")
             return CONSTANT.WEST
 
     def generateMapsNewCords(self, dir: str, cords: list) -> list:
         if dir == CONSTANT.NORTH:
-            return [cords[0], cords[1] + 1]
+            return [cords[0], cords[1] - 1]
         elif dir == CONSTANT.SOUTH:
-            return [cords[0], cords[1] -1]
+            return [cords[0], cords[1] + 1]
         elif dir == CONSTANT.WEST:
-            return[cords[0] - 1, cords[1]]
+            return [cords[0] - 1, cords[1]]
         elif dir == CONSTANT.EAST:
             return [cords[0] + 1, cords[1]]
         else:
             return cords
+
+    def newRate(self, rt: int) -> bool:
+        if random.randint(0, rt) == 0:
+            return False
+        return True
 
 
     def printMap(self):
@@ -85,7 +98,7 @@ class Map:
         self._level[x][y] = rm
 
 
-testing = Map(3, 2)
+testing = Map(3, 3)
 test = Room()
 testing.generateMap()
 testing.printMap()
